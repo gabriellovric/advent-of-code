c2e51acb2c9e7bfbbf6cdf78dbcdb34102cf2a9e
Gabriel Lovric <lovric.gabriel@gmail.com>
2023-12-08 18:40:07 +0100

    parsing done

--- day-5/Solution.java
+++ day-5/Solution.java
@@ -1,14 +1,9 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
-import javax.management.InvalidApplicationException;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
@@ -17,46 +12,54 @@ public class Solution {
             if (args.length != 1 || args[0] == null)
                 throw new FileNotFoundException();
 
-            var scanner = new Scanner(new File(args[0]));
+            var contents = Files.readString(Paths.get(args[0]));
 
-            var seeds = readSeeds(scanner);
-            var seedToSoil = readSeedToSoil(scanner);
+            var parts = contents.split("\\n\\s*\\n");
+            if (parts.length != 8) {
+                throw new IOException("Invalid input file");
+            }
 
-        } catch (Exception e) {
+            var seeds = parts[0].substring(7).split("\s+");
+            var seedToSoil = parseMap(parts[1]);
+            var soilToFertilizer = parseMap(parts[2]);
+            var fertilizerToWater = parseMap(parts[3]);
+            var waterToLight = parseMap(parts[4]);
+            var lightToTemperature = parseMap(parts[5]);
+            var temperatureToHumidity = parseMap(parts[6]);
+            var humidityToLocation = parseMap(parts[7]);
+
+            System.out.println(seeds.length);
+            System.out.println(seedToSoil.length);
+            System.out.println(soilToFertilizer.length);
+            System.out.println(fertilizerToWater.length);
+            System.out.println(waterToLight.length);
+            System.out.println(lightToTemperature.length);
+            System.out.println(temperatureToHumidity.length);
+            System.out.println(humidityToLocation.length);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Usage: java Solution.java <input file>");
+            return;
+        } catch (IOException e) {
             System.out.println("Usage: java Solution.java <input file>");
             return;
         }
+
     }
 
-    private static int[] readSeeds(Scanner scanner) throws Exception {
-        if (!scanner.hasNextLine()) {
-            throw new Exception("Invalid input file");
-        }
-
-        var line = scanner.nextLine();
-        if (!line.startsWith("seeds: ")) {
-            throw new Exception("Invalid input file");
-        }
-
-        var seeds = line.substring(7).split(" ");
-        return Arrays.stream(seeds).mapToInt(Integer::parseInt).toArray();
+    private static long[] mapToIntArray(String[] source) {
+        return Arrays
+                .stream(source)
+                .map(String::trim)
+                .mapToLong(Long::parseLong)
+                .toArray();
     }
 
-    private static int[] readSeedToSoil(Scanner scanner) throws Exception {
-        if (!scanner.hasNextLine()) {
-            throw new Exception("Invalid input file");
-        }
-
-        var line = scanner.nextLine();
-        if (!line.equals("seed-to-soil map:")) {
-            throw new Exception("Invalid input file");
-        }
-
-        while (!line.isEmpty()) {
-            line = scanner.nextLine();
-        }
-
-        var seeds = line.substring(7).split(" ");
-        return Arrays.stream(seeds).mapToInt(Integer::parseInt).toArray();
+    private static long[][] parseMap(String text) {
+        return Arrays
+                .stream(text.split("\r?\n"))
+                .skip(1)
+                .map(x -> mapToIntArray(x.trim().split("\s+")))
+                .toArray(long[][]::new);
     }
 }
